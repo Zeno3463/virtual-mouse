1d90e9c2cd79eddd03a62bf15a4d83d3f719d9f7
Zeno3463 <zenolim3463@gmail.com>
2022-01-01 09:14:05 +0800

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-"# virtual-mouse" 
+# Virtual Mouse
+
+### About
+---
+Control your computer's mouse by your hand motion.
+
+### Features
+---
+- control your mouse movement by moving the index finger of one of your hands in front of your computer's cam.
+- your index finger should be inside the green region shown on the screen
+- perform a left click by moving your thumb close to your index finger
+- close the program by pressing 'q'
+
+### Installation
+---
+1. download the repository
+2. run `main.py`
+
+### Technologies
+---
+1. python
+2. opencv
+3. cvzone
+4. pyautogui
--- main.py
+++ main.py
@@ -4,49 +4,56 @@ import time
 from cvzone.HandTrackingModule import HandDetector
 
 ###### SETTINGS ######
-moveSensitivity: int = 2 # the higher the number, the less sensitive the mouse will be
-rectSize = 100
+rectSize = 200
+xOffset = 100
+yOffset = 170
 
 ###### INITIALIZATION ######
 video = cv2.VideoCapture(0)
-detector = HandDetector(detectionCon=0.8)
+detector = HandDetector(detectionCon=0.1)
 pyautogui.FAILSAFE = False
 
 ###### FUNCTION DECLARATIONS ######
-def calculateMousePositionBasedOnHandPosition(x, y):
+def calculateMousePositionBasedOnHandPosition(x, y, l, t):
 	screenWidth, screenHeight = pyautogui.size()
-	return int((x / rectSize) * screenWidth), int((y / rectSize) * screenHeight)
+	newX = x - l
+	newY = y - t
+	print(newX, newY)
+	return int((newX / rectSize) * screenWidth), int((newY / rectSize) * screenHeight)
 
 def calculateDistanceBtwTwoPoints(x1, y1, x2, y2):
 	return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
+
+def drawRect(img, w, h):
+	newImg = cv2.rectangle(img, ((w//2)-rectSize+xOffset, (h//2)-rectSize+yOffset), ((w//2)+xOffset, (h//2)+yOffset), (0, 255, 0), 2)
+	return newImg, (w//2) - rectSize + xOffset, (h//2) - rectSize + yOffset, (w//2) + rectSize + xOffset, (h//2) + rectSize + yOffset
 
 ###### MAIN LOOP ######
 while True:
 	success, img = video.read()
 	img = cv2.flip(img, 1)
 	hands, img = detector.findHands(img)
-	# img = cv2.Canny(img, 600, 200)
 
 	height, width, _ = img.shape
 
-	img = cv2.rectangle(img, ((width//2)-rectSize, (height//2)-rectSize), ((width//2)+rectSize, (height//2)+rectSize), (0, 255, 0), 2)
-	
+	img, left, top, right, bottom = drawRect(img, width, height)
+
 	# if the hand is found
 	if hands:
 		lmList = hands[0]["lmList"]
 
-		if (width//2)-rectSize <= lmList[8][0] <= (width//2)+rectSize and (height//2)-rectSize <= lmList[8][1] <= (height//2)+rectSize:
+		if left <= lmList[8][0] <= right and top <= lmList[8][1] <= bottom:
 			# get the coordinates of the mouse
-			x, y = calculateMousePositionBasedOnHandPosition(lmList[8][0], lmList[8][1])
+			x, y = calculateMousePositionBasedOnHandPosition(lmList[8][0], lmList[8][1], left, top)
 
-			# # if the user's index finger is close to the user's middle finger (the user clicks)
-			# if 10 < calculateDistanceBtwTwoPoints(lmList[8][0], lmList[8][1], lmList[4][0], lmList[4][1]) < 50:
-			# 	# click the mouse
-			# 	pyautogui.click(x, y)
-			# 	# set a delay to prevent the user from clicking multiple times
-			# 	time.sleep(0.5)
-			# else:
-			pyautogui.moveTo(x, y)
+			# if the user's index finger is close to the user's middle finger (the user clicks)
+			if 10 < calculateDistanceBtwTwoPoints(lmList[8][0], lmList[8][1], lmList[4][0], lmList[4][1]) < 50:
+				# click the mouse
+				pyautogui.click(x, y)
+				# set a delay to prevent the user from clicking multiple times
+				time.sleep(0.5)
+			else:
+				pyautogui.moveTo(x, y)
 
 	cv2.imshow("Video", img)
 	key = cv2.waitKey(1)
